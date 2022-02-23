d33b1996a4525dec9b001bdb3c9f47d7ff38f7cc
TSDilov <cv.dilov@abv.bg>
2022-02-23 18:44:34 +0200

    Added files for simple game that is created after JS Advanced course

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+let state = getState();
+let game = getGameObject();
+
+const availableKeys = [
+    'KeyA',
+    'KeyS',
+    'KeyD',
+    'KeyW',
+    'Space',
+];
+
+document.addEventListener('keydown', (e) => {
+    if (availableKeys.includes(e.code)) {
+        state.keys[e.code] = true;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (availableKeys.includes(e.code)) {
+        state.keys[e.code] = false;
+    }
+});
+
+game.startScreen.addEventListener('click', (e) => {
+    game.startScreen.classList.add('hidden');
+    game.gameScreen.classList.remove('hidden');
+
+    start(state, game);
+});
+
